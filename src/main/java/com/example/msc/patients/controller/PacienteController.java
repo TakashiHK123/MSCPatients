29824e893f313618859100da8d1f3f33991ffb27
TakashiHK123 <takashihk@fiuna.edu.py>
2022-03-28 16:11:12 -0400

    1.1

--- src/main/java/com/example/msc/patients/controller/PacienteController.java
+++ src/main/java/com/example/msc/patients/controller/PacienteController.java
@@ -1,13 +1,15 @@
 package com.example.msc.patients.controller;
 
+import com.example.msc.patients.model.response.PacienteResponse;
+import com.example.msc.patients.model.resquest.PacienteRequest;
 import com.example.msc.patients.service.PacienteService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 
 @CrossOrigin("*")
@@ -20,5 +22,8 @@ public class PacienteController {
     @Autowired
     private PacienteService pacienteService;
 
-
+    @PostMapping
+    public ResponseEntity<?> save(@Valid @RequestBody PacienteRequest body, HttpServletRequest request){
+        return null;
+    }
 }
